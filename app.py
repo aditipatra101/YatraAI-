5769306d96cf673743ef4b82c0ca98217c0d7798
aditipatra101 <aditipatra2024@gmail.com>
2026-04-20 09:51:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import streamlit as st
 import uuid
 import os
 import re
-import chromadb
+import chromadb as chromadb
 import time
 from typing import TypedDict, List
 from sentence_transformers import SentenceTransformer
